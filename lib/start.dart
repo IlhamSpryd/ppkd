0c4772e67b23d3b0eac16902364c0a204de1af64
IlhamSepriyadi <ilhamsepriyadi8@gmail.com>
2025-08-01 14:45:54 +0700

    tugas2Flutter

--- lib/start.dart
+++ lib/start.dart
@@ -6,52 +6,119 @@ class StartWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text(
-          "Profil Saya",
+          "Profile Lengkap",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: const Color.fromARGB(255, 1, 81, 128),
+        backgroundColor: Colors.black,
         actions: [Icon(Icons.menu_sharp, color: Colors.white)],
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 350),
-          Text(
-            "ILHAM SEPRIYADI",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+            const CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blueGrey,
+              child: Icon(Icons.person, size: 50, color: Colors.white),
             ),
-          ),
-          SizedBox(height: 10, width: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.location_on,
-                color: const Color.fromARGB(255, 192, 47, 36),
+            const SizedBox(height: 20),
+            const Text(
+              "ILHAM SEPRIYADI",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Montserrat-BoldItalic',
               ),
-              SizedBox(width: 5, height: 30),
-              Text(
-                'Jakarta, Indonesia',
-                style: TextStyle(fontSize: 15, color: Colors.black),
+            ),
+            const SizedBox(height: 10),
+
+            SizedBox(height: 10),
+            Text(
+              "AI Engineer & Robotics Enthusiast",
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Montserrat-Regular',
               ),
-            ],
-          ),
-          SizedBox(height: 50, width: 10),
-          Column(
-            children: [
-              Text(
-                "Ilham Sepriyadi adalah seorang Mahasiswa yang berambisi menjadi seorang yang ahli pada bidang Artificial Intelligence dan Robotics.",
-                style: TextStyle(fontSize: 15, fontFamily: ""),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  //Container email
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1E1E1E),
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 5),
+                        Icon(Icons.mail, color: Colors.redAccent),
+                        Text(
+                          "ilhamsepriyadi8@gmail.com",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white70,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Container Phone
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1E1E1E),
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 5),
+                        Icon(Icons.phone, color: Colors.green),
+                        Text(
+                          "+62 857-1896-5973",
+                          style: TextStyle(fontSize: 14, color: Colors.white70),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
